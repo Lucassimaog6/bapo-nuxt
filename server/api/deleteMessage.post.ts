7cb020c7b64146b6575214a94df7d87a718ec336
lucassimaog6 <22101918@aluno.cotemig.com.br>
2023-10-08 14:21:01 -0300

    aaa

--- server/api/deleteMessage.post.ts
+++ server/api/deleteMessage.post.ts
@@ -3,8 +3,8 @@ import { prisma } from '~/server/prisma/prisma';
 export default defineEventHandler(async (event) => {
 	const { messageId } = await readBody(event);
 	return await prisma.message.delete({
-		where: {
-			id: parseInt(messageId),
-		},
-	});
+        where: {
+            id: parseInt(messageId),
+        },
+    });
 });
